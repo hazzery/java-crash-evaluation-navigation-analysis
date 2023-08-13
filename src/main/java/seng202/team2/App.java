928964063075e845f9761cc71d13e2d287f377f5
Harrison Parkes <harrydparkes@proton.me>
2023-08-13 16:36:12 +1200

    Arranging imports in App.java

--- src/main/java/seng202/team2/App.java
+++ src/main/java/seng202/team2/App.java
@@ -1,9 +1,10 @@
 package seng202.team2;
 
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Level;
 
+import seng202.team2.services.CSVReader;
 import seng202.team2.gui.MainWindow;
 
 /**
