a1583d877ea930a76939943d92287e218edff114
Ben Moore <benmoore1.work@gmail.com>
2023-08-14 12:53:00 +1200

    Added Cucumber skeleton to test for user queries

---
+++ src/test/java/seng202/team2/cucumber/QueryStepDefs.java
@@ -0,0 +1,27 @@
+package seng202.team2.cucumber;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
+
+/**
+ * Skeleton for what will become the Query tests
+ * @author bmo80
+ */
+public class QueryStepDefs {
+    @Given("All Information Shown")
+    public void allInformationShown() {
+
+    }
+
+    @When("User Asks For Just 2018 Crashes")
+    public void userAsksForJust2018Crashes() {
+
+    }
+
+    @Then("??? Results Are Shown")
+    public void resultsAreShown() {
+        Assertions.assertEquals(2,2);
+    }
+}
