ef02b3e6c6d305de54ddb8deb9b79810b92565e3
FindlayRoyds <findlayroyds@gmail.com>
2023-09-18 15:29:37 +1200

    Created parameterised unit tests for weather enum

--- src/test/java/seng202/team2/unittests/models/WeatherTest.java
+++ src/test/java/seng202/team2/unittests/models/WeatherTest.java
@@ -1,86 +1,73 @@
 package seng202.team2.unittests.models;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import seng202.team2.models.Weather;
 
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 /**
- * Unit test for Weather enum.
+ * Unit test for Severity enum.
  * Tests converting from a string to an enum.
  *
  * @author Findlay Royds
  */
 public class WeatherTest {
     /**
-     * Test getting the enum value FINE from string "Fine"
+     * A parameterised JUnit test to test generating a lighting enum from a string found in the crash data CSV file
+     *
+     * @param weatherString A string that may be encountered when importing lighting enums from the crash data CSV file
+     * @param weatherEnum The expected resulting lighting enum
      */
-    @Test
-    public void fineFromStringTest() {
-        String initialString = "Fine";
-        assertEquals(Weather.FINE, Weather.fromString(initialString));
+    @ParameterizedTest
+    @MethodSource("weatherStringToEnum")
+    void fromStringTest(String weatherString, Weather weatherEnum) {
+        assertEquals(weatherEnum, Weather.fromString(weatherString));
     }
 
     /**
-     * Test getting the enum value SNOW from string "Snow"
+     * A method source for the parametrised lighting string to enum test case
+     *
+     * @return a Stream of Arguments that map different lighting conditions represented as Strings
+     * to their corresponding Lighting enum values.
      */
-    @Test
-    public void snowFromStringTest() {
-        String initialString = "Snow";
-        assertEquals(Weather.SNOW, Weather.fromString(initialString));
-    }
+    private static Stream<Arguments> weatherStringToEnum() {
+        return Stream.of(
+                // Expected case
+                arguments("Fine", Weather.FINE),
+                arguments("Snow", Weather.SNOW),
+                arguments("Hail or sleet", Weather.HAIL_OR_SLEET),
+                arguments("Heavy rain", Weather.HEAVY_RAIN),
+                arguments("Light rain", Weather.LIGHT_RAIN),
+                arguments("Mist or fog", Weather.MIST_OR_FOG),
 
-    /**
-     * Test getting the enum value HAIL_OR_SLEET from string "Hail or Sleet"
-     */
-    @Test
-    public void hailOrSleetFromStringTest() {
-        String initialString = "Hail or Sleet";
-        assertEquals(Weather.HAIL_OR_SLEET, Weather.fromString(initialString));
-    }
+                // Alternative cases
+                arguments("fine", Weather.FINE),
+                arguments("FINE", Weather.FINE),
+                arguments("snow", Weather.SNOW),
+                arguments("SNOW", Weather.SNOW),
+                arguments("hail or sleet", Weather.HAIL_OR_SLEET),
+                arguments("HAIL_OR_SLEET", Weather.HAIL_OR_SLEET),
+                arguments("heavy rain", Weather.HEAVY_RAIN),
+                arguments("HEAVY_RAIN", Weather.HEAVY_RAIN),
+                arguments("light rain", Weather.LIGHT_RAIN),
+                arguments("LIGHT_RAIN", Weather.LIGHT_RAIN),
+                arguments("mist or fog", Weather.MIST_OR_FOG),
+                arguments("MIST_OR_FOG", Weather.MIST_OR_FOG),
 
-    /**
-     * Test getting the enum value HEAVY_RAIN from string "Heavy rain"
-     */
-    @Test
-    public void heavyRainFromStringTest() {
-        String initialString = "Heavy rain";
-        assertEquals(Weather.HEAVY_RAIN, Weather.fromString(initialString));
-    }
-
-    /**
-     * Test getting the enum value LIGHT_RAIN from string "Light rain"
-     */
-    @Test
-    public void lightRainFromStringTest() {
-        String initialString = "Light rain";
-        assertEquals(Weather.LIGHT_RAIN, Weather.fromString(initialString));
-    }
-
-    /**
-     * Test getting the enum value MIST_OR_FOG from string "Mist or Fog"
-     */
-    @Test
-    public void mistOrFogFromStringTest() {
-        String initialString = "Mist or Fog";
-        assertEquals(Weather.MIST_OR_FOG, Weather.fromString(initialString));
-    }
-
-    /**
-     * Test getting the enum value UNKNOWN from an empty string
-     */
-    @Test
-    public void unknownFromEmptyStringTest() {
-        String initialString = "";
-        assertEquals(Weather.UNKNOWN, Weather.fromString(initialString));
-    }
-
-    /**
-     * Test getting the enum value UNKNOWN from an invalid crash severity
-     */
-    @Test
-    public void unknownFromInvalidStringTest() {
-        String initialString = "Pretty bad crash";
-        assertEquals(Weather.UNKNOWN, Weather.fromString(initialString));
+                // Unknown enums
+                arguments("", Weather.UNKNOWN),
+                arguments("Hurricane", Weather.UNKNOWN),
+                arguments(" ", Weather.UNKNOWN),
+                arguments("N/A", Weather.UNKNOWN),
+                arguments("", Weather.UNKNOWN),
+                arguments("NULL", Weather.UNKNOWN),
+                arguments("null", Weather.UNKNOWN),
+                arguments("None", Weather.UNKNOWN)
+        );
     }
 }
