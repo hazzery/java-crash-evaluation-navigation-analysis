997201de049fefb20138b1a147ffd4ba42fcdcd4
FindlayRoyds <findlayroyds@gmail.com>
2023-09-18 15:30:55 +1200

    Created parameterised unit tests for lighting enum

--- src/test/java/seng202/team2/unittests/models/LightingTest.java
+++ src/test/java/seng202/team2/unittests/models/LightingTest.java
@@ -1,68 +1,70 @@
 package seng202.team2.unittests.models;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import seng202.team2.models.Lighting;
 
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 /**
- * Unit test for Lighting enum.
+ * Unit test for Severity enum.
  * Tests converting from a string to an enum.
  *
  * @author Findlay Royds
  */
 public class LightingTest {
     /**
-     * Test getting the enum value OVERCAST from string "Overcast"
+     * A parameterised JUnit test to test generating a lighting enum from a string found in the crash data CSV file
+     *
+     * @param lightingString A string that may be encountered when importing lighting enums from the crash data CSV file
+     * @param lightingEnum The expected resulting lighting enum
      */
-    @Test
-    public void overcastFromStringTest() {
-        String initialString = "Overcast";
-        assertEquals(Lighting.OVERCAST, Lighting.fromString(initialString));
+    @ParameterizedTest
+    @MethodSource("lightingStringToEnum")
+    void fromStringTest(String lightingString, Lighting lightingEnum) {
+        assertEquals(lightingEnum, Lighting.fromString(lightingString));
     }
 
     /**
-     * Test getting the enum value BRIGHT_SUN from string "Bright sun"
+     * A method source for the parametrised lighting string to enum test case
+     *
+     * @return a Stream of Arguments that map different lighting conditions represented as Strings
+     * to their corresponding Lighting enum values.
      */
-    @Test
-    public void brightSunFromStringTest() {
-        String initialString = "Bright sun";
-        assertEquals(Lighting.BRIGHT_SUN, Lighting.fromString(initialString));
-    }
+    private static Stream<Arguments> lightingStringToEnum() {
+        return Stream.of(
+                // Expected case
+                arguments("Overcast", Lighting.OVERCAST),
+                arguments("Bright sun", Lighting.BRIGHT_SUN),
+                arguments("Twilight", Lighting.TWILIGHT),
+                arguments("Dark", Lighting.DARK),
 
-    /**
-     * Test getting the enum value TWILIGHT from string "Twilight"
-     */
-    @Test
-    public void twilightFromStringTest() {
-        String initialString = "Twilight";
-        assertEquals(Lighting.TWILIGHT, Lighting.fromString(initialString));
-    }
+                // Alternative cases
+                arguments("OverCast", Lighting.OVERCAST),
+                arguments("overcast", Lighting.OVERCAST),
+                arguments("Bright Sun", Lighting.BRIGHT_SUN),
+                arguments("bright sun", Lighting.BRIGHT_SUN),
+                arguments("Bright_sun", Lighting.BRIGHT_SUN),
+                arguments("BRIGHT_SUN", Lighting.BRIGHT_SUN),
+                arguments("TwiLight", Lighting.TWILIGHT),
+                arguments("twilight", Lighting.TWILIGHT),
+                arguments("TWILIGHT", Lighting.TWILIGHT),
+                arguments("dark", Lighting.DARK),
+                arguments("DARK", Lighting.DARK),
 
-    /**
-     * Test getting the enum value DARK from string "Dark"
-     */
-    @Test
-    public void darkFromStringTest() {
-        String initialString = "Dark";
-        assertEquals(Lighting.DARK, Lighting.fromString(initialString));
-    }
-
-    /**
-     * Test getting the enum value UNKNOWN from an empty string
-     */
-    @Test
-    public void unknownFromEmptyStringTest() {
-        String initialString = "";
-        assertEquals(Lighting.UNKNOWN, Lighting.fromString(initialString));
-    }
-
-    /**
-     * Test getting the enum value UNKNOWN from an invalid lighting condition
-     */
-    @Test
-    public void unknownFromInvalidStringTest() {
-        String initialString = "Rather dark eh";
-        assertEquals(Lighting.UNKNOWN, Lighting.fromString(initialString));
+                // Unknown enums
+                arguments("", Lighting.UNKNOWN),
+                arguments("rather dark", Lighting.UNKNOWN),
+                arguments(" ", Lighting.UNKNOWN),
+                arguments("N/A", Lighting.UNKNOWN),
+                arguments("", Lighting.UNKNOWN),
+                arguments("NULL", Lighting.UNKNOWN),
+                arguments("null", Lighting.UNKNOWN),
+                arguments("None", Lighting.UNKNOWN)
+        );
     }
 }
