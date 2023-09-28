8354b136c3eadd993c7326dc92b1122ecbafb0ea
Harrison Parkes <harrydparkes@proton.me>
2023-09-28 14:55:20 +1300

    No longer showing Unknown region in drop down

--- src/main/java/seng202/team2/controller/ButtonBarController.java
+++ src/main/java/seng202/team2/controller/ButtonBarController.java
@@ -116,7 +116,7 @@ public class ButtonBarController {
      * Sets the regions in the regions menu
      */
     private void setRegions() {
-        for (Region region : Region.values()) {
+        for (Region region : Region.regions()) {
             CheckMenuItem regionItem = new CheckMenuItem(region.displayValue());
             regionItem.setId(region.name());
             Regions.getItems().add(regionItem);
