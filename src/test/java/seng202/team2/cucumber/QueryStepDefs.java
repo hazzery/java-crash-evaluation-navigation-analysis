12f615c0e53a373669a101513c8363774028a925
James Lanigan <James.lanigan04@gmail.com>
2023-10-08 14:45:27 +1300

    Made case for applying no filters - currently should pass even if incorrect

--- src/test/java/seng202/team2/cucumber/QueryStepDefs.java
+++ src/test/java/seng202/team2/cucumber/QueryStepDefs.java
@@ -4,15 +4,35 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
+import seng202.team2.database.CrashDao;
+import seng202.team2.database.QueryBuilder;
+import seng202.team2.models.Crash;
+
+import java.util.List;
 
 /**
  * Skeleton for what will become the Query tests
  * @author bmo80
+ * @author James Lanigan
  */
 public class QueryStepDefs {
-    @Given("I have the car button selected")
-    public void allInformationShown() {
+    QueryBuilder queryTester;
+    List<Crash> queryResult;
+    CrashDao testDao = new CrashDao();
 
+    @Given("I have no filters selected")
+    public void noFilters() {
+        queryTester = new QueryBuilder();
+    }
+
+    @When("I press apply")
+    public void applyQuery() {
+        queryResult = testDao.queryDatabase(queryTester.getQuery());
+    }
+
+    @Then("All results in database are shown")
+    public void allRowsShown() {
+        Assertions.assertTrue(0<queryResult.size());
     }
 
     @When("User Asks For Just 2018 Crashes")
--- src/test/resources/features/Query.feature
+++ src/test/resources/features/Query.feature
@@ -1,4 +1,9 @@
 Feature: Query Test
+  Scenario: No filters
+    Given I have no filters selected
+    When I press apply
+    Then All results in database are shown
+
   Scenario: Filter with car
     Given I have the car button selected
     When I press apply
