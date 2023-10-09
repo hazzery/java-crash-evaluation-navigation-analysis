d6265c3914651407a4ac67b01b70aae4f5f41cab
Ben Moore <benmoore1.work@gmail.com>
2023-10-10 12:08:47 +1300

    Added query acceptance test

--- src/test/java/seng202/team2/cucumber/QueryStepDefs.java
+++ src/test/java/seng202/team2/cucumber/QueryStepDefs.java
@@ -5,9 +5,12 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
 import seng202.team2.database.CrashDao;
+import seng202.team2.database.DbAttributes;
 import seng202.team2.database.QueryBuilder;
 import seng202.team2.models.Crash;
+import seng202.team2.models.Severity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -33,6 +36,30 @@ public class QueryStepDefs {
     @Then("All results in database are shown")
     public void allRowsShown() {
         Assertions.assertEquals(820467,queryResult.size());
+    }
+
+    @Given("User has Person and fatal severity selected")
+    public void fatalPersonFilter() {
+        queryTester = new QueryBuilder();
+        ArrayList<String> severityTest = new ArrayList<String>();
+        severityTest.add("FATAL");
+        queryTester.orString(severityTest, DbAttributes.SEVERITY);
+
+        ArrayList<DbAttributes> pedestrianTest = new ArrayList<DbAttributes>();
+        pedestrianTest.add(DbAttributes.PEDESTRIAN);
+        queryTester.orVehicle(pedestrianTest);
+    }
+    @Then("All results shown involve a pedestrian and a fatality")
+    public void allFatalPedestrianResults() {
+        boolean valid = true;
+        for (Crash crash: queryResult) {
+            if (!(crash.severity().equals(Severity.FATAL)) ||
+                    !(crash.vehicles().containsKey(DbAttributes.PEDESTRIAN))) {
+                valid = false;
+                break;
+            }
+        }
+        Assertions.assertTrue(valid);
     }
 
     @When("User Asks For Just 2018 Crashes")
