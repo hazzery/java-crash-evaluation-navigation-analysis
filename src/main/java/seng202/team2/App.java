11e5f9e9ccf19fcbc83cc32b29dfb8ed522991e9
Harrison Parkes <harrydparkes@proton.me>
2023-10-15 16:31:30 +1300

    removed logger from App.java

--- src/main/java/seng202/team2/App.java
+++ src/main/java/seng202/team2/App.java
@@ -1,7 +1,5 @@
 package seng202.team2;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import seng202.team2.models.Crashes;
 
 /**
@@ -9,8 +7,6 @@ import seng202.team2.models.Crashes;
  * @author seng202 teaching team
  */
 public class App {
-    private static final Logger log = LogManager.getLogger(App.class);
-
     /**
      * Entry point which runs the javaFX application
      * Also shows off some different logging levels
