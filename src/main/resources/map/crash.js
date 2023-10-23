4a42714576c8df1d3be07d70ebf54d1194637913
Harry Parkes <harrydparkes@proton.me>
2023-10-23 17:35:05 +1300

    Added javascript crash class

---
+++ src/main/resources/map/crash.js
@@ -0,0 +1,14 @@
+export default class Crash {
+    /**
+     * Creates a new crash object
+     *
+     * @param {number} latitude
+     * @param {number} longitude
+     * @param {int} severity
+     */
+    constructor(latitude, longitude, severity) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.severity = severity;
+    }
+}
